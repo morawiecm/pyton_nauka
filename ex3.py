a14c794782b13eb9e96c39590924532d8c149e92
Mariusz Morawiec <mariusz.morawiec@globallogic.com>
2018-10-27 09:35:32 +0200

    cwiczenie 3 - liczby i dzialania algebraiczne

---
+++ ex3.py
@@ -0,0 +1,23 @@
+print("Policze teraz poglowie mojego drobiu")
+
+print("Koguty", 25 + 50 / 6)
+print("Kury", 100 - 25 * 3 % 4)
+
+print("Teraz policze jaja:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Czy to prawda, ze 3+2/<5-7")
+
+print(3 + 2 < 5 - 7)
+
+print("Ile to jest 3+2", 3 + 2)
+print("Ile to jest 5-7", 5 - 7)
+
+print("Oj to dlatego daje False")
+
+print("Pocwiczymy jeszcze troche")
+
+print("Czy to jest wieksze?", 5 > -2)
+print("Czy to jest wieksze lub rowne", 5 >= -2)
+print("Czy to jest mniejsze lub rowne", 5 <= -2)
