9ddc5bb782d73ad68d4a79e36e093aee012a8b49
Mariusz Morawiec <mariusz.morawiec@globallogic.com>
2018-10-28 06:20:46 +0100

    cwiczenie 5 zmienne cd

---
+++ ex5.py
@@ -0,0 +1,19 @@
+my_name = 'Zed A. Shaw'
+my_age = 35  # nie klamie
+my_height = 190  # centymetrow
+my_weight = 80  # kilogramow
+my_eyes = 'Niebieske'
+my_teeth = 'Biale'
+my_hair = 'Brazowe'
+
+print(f"Porozmawiajmy o {my_name}.")
+print(f"Ma {my_height} centymetrow wzrostu")
+print(f"Wazy {my_weight} kilogramow")
+print("Tak nie wazy duzo")
+print(f"Ma {my_eyes} oczy i {my_hair} wlosy")
+print(
+    f"Jegp zeby sa zazwyczaj {my_teeth}, w zaleznosci od ilosci wypitej kawy")
+
+# ta linia jest trudna wiec wpisuj ja uwaznie
+total = my_age + my_height + my_weight
+print(f"Jesli dodatm {my_age}, {my_height} i {my_weight}, otrzymam {total} ")
