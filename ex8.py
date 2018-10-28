033c52d587411209ced408c2221753c9571e7fb7
Mariusz Morawiec <mariusz.morawiec@globallogic.com>
2018-10-28 06:54:26 +0100

    cwiczenie 8 lancuhy i funcka format

---
+++ ex8.py
@@ -0,0 +1,7 @@
+formater = "{} {} {} {}"
+print(formater.format(1, 2, 3, 4))
+print(formater.format("jeden", "dwa", "trzy", "cztery"))
+print(formater.format(True, False, False, True))
+print(formater.format(formater, formater, formater, formater))
+print(formater.format("Sprobuj tutaj", "Wpisac wlasny tekst",
+                      "Moze wiersz", "Albo piosenek o leku"))
