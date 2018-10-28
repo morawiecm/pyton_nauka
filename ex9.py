5340af7c2ddc7541919925cbbb061abdd58b2123
Mariusz Morawiec <mariusz.morawiec@globallogic.com>
2018-10-28 07:01:41 +0100

    cwiczenie 9 - formatowanie lancuchow tekstowych

---
+++ ex9.py
@@ -0,0 +1,12 @@
+days = "Pon Wt Sr Czw Pt Sob Niedz"
+months = "Sty\nLut\nMar\nKwi\nMaj\nCze\nLip\nSie"
+
+print("Oto dni tygodnia:", days)
+print("Oto miesiace", months)
+
+print("""
+Tutaj cos sie dzieje.
+Uzywamy trzech podwojnych cudzyslowow
+Bedziemy w stanie wpisac dowolnie duzo tekstu
+Jesli chcemy, nawet 4 linie albo 5 lub 6 
+""")
