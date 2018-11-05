e10f0c127bb3140f9e9418122f9b84d6cb36ed3f
Mariusz Morawiec <mariusz.morawiec@globallogic.com>
2018-11-05 20:47:50 +0100

    czwiecznie 13 - wprowadzanie danych do programu za pomoca argumentu wywolania argv

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print("ten skrypt nazywa sie: ", script)
+print("Pierwsza zmienna to: ", first)
+print("Druga zmienna to: ", second)
+print("Trzecia zmienna to: ", third)
