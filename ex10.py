7331cb4ca358844e106828c179b37b2e8d99887a
Mariusz Morawiec <mariusz.morawiec@globallogic.com>
2018-11-05 20:18:00 +0100

    cwiczenie 10 - formatowanie lancuchow tekstowych

---
+++ ex10.py
@@ -0,0 +1,15 @@
+tabby_cat = "\tJestem tabulatorem."
+persian_cat = "Jestem podzialem \nlini"
+backslash_cat = " Jestem sobie \\jakims \\ kotem"
+
+fat_cat = """
+Zrobie liste:
+\t* Jedzeniedla kotka
+\t* Rybki
+\t* Kocimietka\n\t* Trawa
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
