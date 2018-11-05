c2068a91832b5d57169ee063e14d037ece3e0c67
Mariusz Morawiec <mariusz.morawiec@globallogic.com>
2018-11-05 20:35:53 +0100

    cwiczenie 11  - wprowadzanie danych metoda input

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print("Ile masz lat?", end=' ')
+age = input()
+print("Ile masz wzrostu?", end=' ')
+height = input()
+print("Ile wazysz?", end=' ')
+weight = input()
+
+print(f"Wiec masz {age}lat, masz {height}cm wzrostu i wazysz {weight}kg")
