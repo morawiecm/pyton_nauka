3ab2ec895a942262b723fd3647ea74e139fad08f
Mariusz Morawiec <mariusz.morawiec@globallogic.com>
2018-11-07 14:34:53 +0100

    cwiczenie 29 - instrukca warunkowa if

---
+++ ex29.py
@@ -0,0 +1,14 @@
+people = 30
+cats = 30
+dogs = 15
+
+if people < cats:
+    print("Zbyt duzo kotow, swiat jest skazany na zaglade!")
+
+if people > cats:
+    print("Nie za duzo kotów! Swiat jest ocalony")
+
+if people < dogs:
+    print("Swiat sie slini")
+
+if people
